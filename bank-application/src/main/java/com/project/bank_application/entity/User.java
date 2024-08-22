ac5d89bcf5ae0189634c6584e0818f8c1c6ea6ef
Pichau <igorof06@outlook.com>
2024-08-22 12:04:35 -0300

    Creation of User entity and repository

---
+++ bank-application/src/main/java/com/project/bank_application/entity/User.java
@@ -0,0 +1,191 @@
+package com.project.bank_application.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    private String otherName;
+
+    private String gender;
+
+    private String address;
+
+    private String stateOfOrigin;
+
+    private String accountNumber;
+
+    private BigDecimal accountBalance;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private String alternativePhone;
+
+    private String status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+
+    public User() {}
+
+    public User(Long id, String name, String lastName, String otherName,
+                String gender, String address, String stateOfOrigin,
+                String accountNumber, BigDecimal accountBalance, String email,
+                String phoneNumber, String alternativePhone, String status,
+                LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.otherName = otherName;
+        this.gender = gender;
+        this.address = address;
+        this.stateOfOrigin = stateOfOrigin;
+        this.accountNumber = accountNumber;
+        this.accountBalance = accountBalance;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.alternativePhone = alternativePhone;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getOtherName() {
+        return otherName;
+    }
+
+    public void setOtherName(String otherName) {
+        this.otherName = otherName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStateOfOrigin() {
+        return stateOfOrigin;
+    }
+
+    public void setStateOfOrigin(String stateOfOrigin) {
+        this.stateOfOrigin = stateOfOrigin;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(BigDecimal accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAlternativePhone() {
+        return alternativePhone;
+    }
+
+    public void setAlternativePhone(String alternativePhone) {
+        this.alternativePhone = alternativePhone;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+}
---
+++ bank-application/src/main/java/com/project/bank_application/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.project.bank_application.repository;
+
+import com.project.bank_application.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Boolean existsByEmail(String email);
+
+}
