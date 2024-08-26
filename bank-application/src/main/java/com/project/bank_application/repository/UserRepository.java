f62c2be01cad368632925aa14e9a79ba08acc540
Pichau <igorof06@outlook.com>
2024-08-26 11:05:21 -0300

    Implement methods in UserRepository: exists and findBy AccountNumber

--- bank-application/src/main/java/com/project/bank_application/repository/UserRepository.java
+++ bank-application/src/main/java/com/project/bank_application/repository/UserRepository.java
@@ -7,4 +7,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByEmail(String email);
 
+    boolean existsByAccountNumber(String accountNumber);
+
+    User findByAccountNumber(String accountNumber);
+
 }
