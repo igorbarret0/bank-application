ab5cfc7e14e8357730d1d5ef90a3367528e2da2d
Pichau <igorof06@outlook.com>
2024-08-26 09:44:44 -0300

    Creation of email service

---
+++ bank-application/src/main/java/com/project/bank_application/service/EmailService.java
@@ -0,0 +1,11 @@
+package com.project.bank_application.service;
+
+import com.project.bank_application.dtos.EmailDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface EmailService {
+
+    void sendEmail(EmailDetails emailDetails);
+
+}
---
+++ bank-application/src/main/java/com/project/bank_application/service/impl/EmailServiceImpl.java
@@ -0,0 +1,39 @@
+package com.project.bank_application.service.impl;
+
+import com.project.bank_application.dtos.EmailDetails;
+import com.project.bank_application.service.EmailService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    private JavaMailSender javaMailSender;
+
+    public EmailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+    @Override
+    public void sendEmail(EmailDetails emailDetails) {
+
+        try {
+
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(senderEmail);
+            mailMessage.setTo(emailDetails.getRecipient());
+            mailMessage.setText(emailDetails.getMessageBody());
+            mailMessage.setSubject(emailDetails.getSubject());
+
+            javaMailSender.send(mailMessage);
+            System.out.println("Mail sent successfully");
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
