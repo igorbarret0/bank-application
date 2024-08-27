e80c314d75a8897746e2647fdf15ef33e83c5295
Pichau <igorof06@outlook.com>
2024-08-27 13:29:29 -0300

    added application documentation

--- bank-application/src/main/java/com/project/bank_application/BankApplication.java
+++ bank-application/src/main/java/com/project/bank_application/BankApplication.java
@@ -1,9 +1,20 @@
 package com.project.bank_application;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Bank Application",
+				description = "Backend Rest APIs simulating the operations of a bank",
+				version = "v1.0"
+		)
+)
 public class BankApplication {
 
 	public static void main(String[] args) {
--- bank-application/src/main/java/com/project/bank_application/controller/UserController.java
+++ bank-application/src/main/java/com/project/bank_application/controller/UserController.java
@@ -2,12 +2,16 @@ package com.project.bank_application.controller;
 
 import com.project.bank_application.dtos.*;
 import com.project.bank_application.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
+@Tag(name = "User Account Management APIs")
 public class UserController {
 
     private UserService userService;
@@ -17,6 +21,14 @@ public class UserController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Create New User Account",
+            description = "Creating a new user and assigning an account ID"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 created"
+    )
     public ResponseEntity<BankResponse> createAccount(@RequestBody UserRequest request) {
 
         var newUser = userService.createAccount(request);
@@ -24,6 +36,14 @@ public class UserController {
     }
 
     @GetMapping("/balanceEnquiry")
+    @Operation(
+            summary = "Balance enquiry",
+            description = "Given an account number, check how much the user has"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 0K"
+    )
     public ResponseEntity<BankResponse> balanceEnquiry(@RequestBody EnquiryRequest request) {
 
         var response = userService.balanceEnquiry(request);
@@ -31,6 +51,14 @@ public class UserController {
     }
 
     @GetMapping("/nameEnquiry")
+    @Operation(
+            summary = "Name Enquiry",
+            description = "Given an account number, return the name owner of the account"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status OK"
+    )
     public ResponseEntity<String> nameEnquiry(@RequestBody EnquiryRequest request) {
 
         var response = userService.nameEnquiry(request);
@@ -38,6 +66,14 @@ public class UserController {
     }
 
     @PostMapping("/credit")
+    @Operation(
+            summary = "Credit a account",
+            description = "Given an account number, if account exists, is credited"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status OK"
+    )
     public ResponseEntity<BankResponse> creditAccount(@RequestBody CreditDebitRequest request) {
 
         var response = userService.creditAccount(request);
@@ -45,6 +81,14 @@ public class UserController {
     }
 
     @PostMapping("/debit")
+    @Operation(
+            summary = "Debit a account",
+            description = "If the account exists and has sufficient funds, it will be debited."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status OK"
+    )
     public ResponseEntity<BankResponse> debitAccount(@RequestBody CreditDebitRequest request) {
 
         var response = userService.debitAccount(request);
@@ -52,6 +96,14 @@ public class UserController {
     }
 
     @PostMapping("/transfer")
+    @Operation(
+            summary = "Transfer Between Accounts",
+            description = "This endpoint allows users to transfer funds between two bank accounts"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status OK"
+    )
     public ResponseEntity<BankResponse> transfer(@RequestBody TransferRequest request) {
 
         var response = userService.transfer(request);
